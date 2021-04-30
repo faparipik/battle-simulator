e5ec0e4e9af0cd5477fb7af421fe0317c1f830ff
faparipik <bruncevicfaruk@gmail.com>
2021-05-01 00:41:41 +0200

    dotenv and joi for validating enviroments

---
+++ src/config/env.config.ts
@@ -0,0 +1,27 @@
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
+
+dotenv.config();
+
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string()
+      .valid('production', 'development', 'test')
+      .required(),
+    PORT: Joi.number().default(3000),
+    ELASTIC_APM_SERVER_URL: Joi.string().uri().required(),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: 'key' } })
+  .validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+export default {
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+};
