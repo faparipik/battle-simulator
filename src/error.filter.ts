df1f6f81a1c1fedd5c269c55769aeaf62d8c8141
faparipik <bruncevicfaruk@gmail.com>
2021-05-01 02:55:42 +0200

    validation error handling

--- src/error.filter.ts
+++ src/error.filter.ts
@@ -8,6 +8,13 @@ import {
 import { Request, Response } from 'express';
 import * as apm from 'elastic-apm-node';
 
+interface responseJsonInterface {
+  statusCode: number;
+  timestamp: string;
+  path: string;
+  message?: any;
+}
+
 @Catch()
 export class ErrorFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
@@ -20,15 +27,27 @@ export class ErrorFilter implements ExceptionFilter {
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
+    let responseJson: responseJsonInterface = {
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    };
+
+    // Validation Error
+    if (exception.response && exception.response.statusCode === 400) {
+      const { statusCode, message } = exception.response;
+      responseJson = {
+        ...responseJson,
+        statusCode,
+        message,
+      };
+    }
+
     apm.captureError(exception, {
       request,
       response,
     });
 
-    response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-    });
+    response.status(status).json(responseJson);
   }
 }
