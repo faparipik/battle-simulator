028bd675d5ce40919cc77ca6e0f12a9a3b3dc4eb
faparipik <bruncevicfaruk@gmail.com>
2021-05-01 18:01:38 +0200

    Added helmet and compression

--- src/config/global-configs.ts
+++ src/config/global-configs.ts
@@ -1,7 +1,12 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
+
 import { ErrorFilter } from './error.filter';
 
 const createGlobalSetup = (app: INestApplication) => {
+  app.use(helmet());
+  app.use(compression());
   app.useGlobalFilters(new ErrorFilter());
   app.useGlobalPipes(
     new ValidationPipe({
